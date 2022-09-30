a67cec2ee7d24db08659d10c9c0ab3772ca53c27
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-09-30 12:28:07 +0200

    Page Accomodation : modifications des étoiles et des tags + Suppression de code inutile

--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -19,7 +19,7 @@ function Accomodation() {
                 <div>
                     <h1 id="intro-title">{title}</h1>
                     <h2 id="intro-location">{location}</h2>
-                    <div>
+                    <div id="tags">
                         {tags.map((tag) => {
                             return (
                                 <span className="tag" key={`${tag}`}>
@@ -35,13 +35,29 @@ function Accomodation() {
                     <div id="host-stars">
                         {range.map((rangeElem) =>
                             rating >= rangeElem ? (
-                                <span className="star-red" key={rangeElem.toString()}>
-                                    <i className="fa-solid fa-star"></i>
-                                </span>
+                                <svg
+                                    key={rangeElem.toString()}
+                                    className="star"
+                                    viewBox="0 0 30 30"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        className="star-red"
+                                        d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z"
+                                    />
+                                </svg>
                             ) : (
-                                <span className="star-grey" key={rangeElem.toString()}>
-                                    <i className="fa-solid fa-star"></i>
-                                </span>
+                                <svg
+                                    key={rangeElem.toString()}
+                                    className="star"
+                                    viewBox="0 0 30 30"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        className="star-grey"
+                                        d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z"
+                                    />
+                                </svg>
                             )
                         )}
                     </div>
