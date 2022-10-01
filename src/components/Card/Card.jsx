70d9bc2190f13b01d434475134585dc090632306
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-10-01 14:15:09 +0200

    Ajout d'une ombre pour mieux voir les titres des logements

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,7 @@ function Card({ cover, title }) {
     return (
         <div className="accomodation-card">
             <img src={cover} alt="freelance" />
+            <div id="card-bg"></div>
             <h2>{title}</h2>
         </div>
     );
