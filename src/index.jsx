db80254e7c87b0b2296b08de3c3f01225383085e
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-10-07 10:27:38 +0200

    Ajout d'une fonction pour remonter en haut quand on change de page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import { React, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./style/style.css";
 import Home from "./pages/Home/Home.jsx";
 import Accomodation from "./pages/Accomodation/Accomodation";
@@ -9,16 +9,31 @@ import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
 import Error from "./pages/Error/Error";
 
+/**
+ * When the pathname changes, scroll to the top of the page.
+ * @returns null
+ */
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route 
-                path="/accomodation/:id" 
+            <Route
+                path="/accomodation/:id"
                 render={(props) => <Accomodation {...props} />}
-                element={<Accomodation />} 
+                element={<Accomodation />}
             />
             <Route path="/about" element={<About />} />
             <Route path="*" element={<Error />} />
